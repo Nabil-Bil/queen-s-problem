7bcc1b40dce2232e125433062961ef1aa824c349
Nabil-Bil <nabil.radjai@etu.usthb.dz>
2023-04-29 02:10:27 +0100

    Ui finished

--- src/main/java/com/example/queens_problem/HomeController.java
+++ src/main/java/com/example/queens_problem/HomeController.java
@@ -12,6 +12,7 @@ import javafx.scene.shape.Ellipse;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.text.DecimalFormat;
 import java.util.Objects;
 
 
@@ -81,6 +82,16 @@ public class HomeController {
 
     int rate=60;
 
+    double muationRate=0.05;
+    double c1=1;
+    double c2=1;
+    double theta=1;
+
+    int populationSizePSO=100;
+    int maxGenerationPSO=100;
+
+
+
     @FXML
     void onRunButtonClick(MouseEvent event) {
         try {
@@ -116,7 +127,7 @@ public class HomeController {
         } else if(selectedAlgorithm == Algorithm.GA){
             nQueens = new NQueensGA(chessSize,populationSize,maxGenerations,rate);
         }else{
-            nQueens=new NQueensPSO(chessSize);
+            nQueens=new NQueensPSO(chessSize,theta,c1,c2,populationSizePSO,maxGenerationPSO,muationRate);
         }
         Chess.drawChess(chessSize, chess);
         Result result = nQueens.solveAndGetResult();
@@ -177,8 +188,16 @@ public class HomeController {
         circle.setLayoutY(y);
     }
     @FXML
-    void changeParams() throws IOException {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("params.fxml"));
+    void changeParams()  throws IOException{
+        if(selectedAlgorithm==Algorithm.PSO){
+            changeParamsPSO();
+        }else{
+            changeParamsGA();
+        }
+    }
+
+    void changeParamsGA()throws IOException{
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("GaPopup.fxml"));
         Parent root = loader.load();
         Scene scene = new Scene(root);
         Stage stage = new Stage();
@@ -212,6 +231,78 @@ public class HomeController {
             populationLabel.setText(String.valueOf(populationSize));
 
         });
+        Label exit=(Label) scene.lookup("#exit");
+        exit.setOnMouseClicked(e->{
+            stage.close();
+        });
+    }
+    void changeParamsPSO()throws IOException{
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("PSOPopup.fxml"));
+        Parent root = loader.load();
+        Scene scene = new Scene(root);
+        Stage stage = new Stage();
+        stage.setScene(scene);
+        stage.setTitle("Change Parameters");
+        stage.show();
+        Slider generationSlider = (Slider) scene.lookup("#generationPsoSlider");
+        Slider populationSlider = (Slider) scene.lookup("#populationPsoSlider");
+        Slider mutationRateSlider = (Slider) scene.lookup("#mutationRateSlider");
+        Slider learningRateC1Slider=(Slider)scene.lookup("#learningRateC1Slider");
+        Slider learningRateC2Slider=(Slider)scene.lookup("#learningRateC2Slider");
+        Slider thetaSlider=(Slider)scene.lookup("#thetaSlider");
+
+        Label learningRateC1Label=(Label)scene.lookup("#learningRateC1Label");
+        Label learningRateC2Label=(Label)scene.lookup("#learningRateC2Label");
+        Label thetaLabel=(Label)scene.lookup("#thetaLabel");
+        DecimalFormat df=new DecimalFormat("#.##");
+        Label generationLabel=(Label) scene.lookup("#generationPsoLabel");
+        Label mutationRateLabel=(Label) scene.lookup("#mutationRateLabel");
+        Label populationLabel=(Label) scene.lookup("#populationPsoLabel");
+        generationSlider.setValue(maxGenerationPSO);
+        populationSlider.setValue(populationSizePSO);
+        mutationRateSlider.setValue(muationRate);
+        thetaSlider.setValue(theta);
+        learningRateC1Slider.setValue(c1);
+        learningRateC2Slider.setValue(c2);
+        generationLabel.setText(String.valueOf(maxGenerationPSO));
+        populationLabel.setText(String.valueOf(populationSizePSO));
+        thetaLabel.setText(String.valueOf(df.format(theta)));
+        learningRateC1Label.setText(String.valueOf(df.format(c1)));
+        learningRateC2Label.setText(String.valueOf(df.format(c2)));
+        mutationRateLabel.setText((int)(muationRate*100) +"%");
+        generationSlider.valueProperty().addListener((observable, oldValue, newValue) -> {
+            maxGenerationPSO= newValue.intValue();
+            generationLabel.setText(String.valueOf(maxGenerationPSO));
+
+        });
+        mutationRateSlider.valueProperty().addListener((observable, oldValue, newValue) -> {
+            muationRate= newValue.doubleValue();
+            mutationRateLabel.setText((int)(muationRate*100) +"%");
+
+        });
+        populationSlider.valueProperty().addListener((observable, oldValue, newValue) -> {
+            populationSizePSO= newValue.intValue();
+            populationLabel.setText(String.valueOf(populationSizePSO));
+
+        });
+
+        thetaSlider.valueProperty().addListener((observable, oldValue, newValue) -> {
+            theta= newValue.doubleValue();
+            thetaLabel.setText(String.valueOf(df.format(theta)));
+
+        });
+        learningRateC1Slider.valueProperty().addListener((observable, oldValue, newValue) -> {
+            c1= newValue.doubleValue();
+            learningRateC1Label.setText(String.valueOf(df.format(c1)));
+
+        });
+        learningRateC2Slider.valueProperty().addListener((observable, oldValue, newValue) -> {
+            c2= newValue.doubleValue();
+            learningRateC2Label.setText(String.valueOf(df.format(c2)));
+
+        });
+
+
         Label exit=(Label) scene.lookup("#exit");
         exit.setOnMouseClicked(e->{
             stage.close();
--- src/main/java/com/example/queens_problem/logic/NQueensPSO.java
+++ src/main/java/com/example/queens_problem/logic/NQueensPSO.java
@@ -3,25 +3,24 @@ package com.example.queens_problem.logic;
 import java.util.*;
 
 public class NQueensPSO extends NQueens{
-    float theta;
+    double theta;
     double c1;
     double c2;
 
     int populationSize;
     int iterationNumber;
 
+    double mutationRate;
 
 
-    public NQueensPSO(int n){
-        super(n);
-    }
-    public NQueensPSO(int n,int theta,double c1, double c2,int populationSize,int iterationNumber) {
+    public NQueensPSO(int n,double theta,double c1, double c2,int populationSize,int iterationNumber,double mutationRate) {
         super(n);
         this.theta=theta;
         this.c1=c1;
         this.c2=c2;
         this.populationSize=populationSize;
         this.iterationNumber=iterationNumber;
+        this.mutationRate=mutationRate;
     }
     private ArrayList<Particle> generateRandomParticles(int populationSize) {
         ArrayList<Particle> population = new ArrayList<>(populationSize);
@@ -86,44 +85,42 @@ public class NQueensPSO extends NQueens{
     return newPosition;
     }
 
-    private Particle rechercheTabou(Particle particle, int maxIterations) {
+    private Particle hillClimbing(Particle particle) {
 
-        int iter=0;
-        ArrayList<boolean[][]> tabuList=new ArrayList<>();
         Particle bestNeighbour=particle.clone();
-        while (iter<maxIterations){
                 ArrayList<Particle> neighbours=null;
-                neighbours=generateNeighbours(bestNeighbour,tabuList);
+                neighbours=generateNeighbours(bestNeighbour);
                 neighbours.sort(Comparator.comparingInt(Particle::getFitnessValue));
                 Particle neighbour=neighbours.get(0);
                     if(neighbour.getFitnessValue() < bestNeighbour.getFitnessValue()){
                         bestNeighbour = neighbour;
                     }
-                    tabuList.add(neighbour.position);
-            iter++;
-        }
         return bestNeighbour;
     }
 
 
-    private ArrayList<Particle> generateNeighbours(Particle currentSolution,ArrayList<boolean[][]> tabuList) {
+    private ArrayList<Particle> generateNeighbours(Particle currentSolution) {
         ArrayList<Particle> neighbours=new ArrayList<>();
         for(int i=0;i<n;i++){
+
             int col=0;
             while (!currentSolution.position[i][col]){
                 col++;
             }
-            Particle neighbour = currentSolution.clone();
-            int newCol = (int) (Math.random() * n);
-            while (newCol ==col) {
-                newCol= (int) (Math.random() * n);
+            int bestCol=col;
+            int bestColConflicts=countAttacks(currentSolution.position,i,col);
+            for(int j=0;j<n;j++){
+                int conflicts=countAttacks(currentSolution.position,i,j);
+                if(bestColConflicts>conflicts){
+                    bestCol=j;
+                    bestColConflicts=conflicts;
+                }
             }
+            Particle neighbour = currentSolution.clone();
             neighbour.position[i][col] = false;
-            neighbour.position[i][newCol] = true;
+            neighbour.position[i][bestCol] = true;
             neighbour.fitnessValue=calculateFitness(neighbour.position);
             neighbours.add(neighbour);
-//            if(!isTabou(neighbour,tabuList))
-//                neighbours.add(new Node(neighbour,0,calculateFitness(neighbour)));
         }
         return neighbours;
 
@@ -146,24 +143,9 @@ public class NQueensPSO extends NQueens{
         particule.position[row][currentQueenPos] = false;
         particule.position[row][newQueenPos] = true;
     }
-    private boolean isTabou(boolean[][] neighbour, ArrayList<boolean[][]> tabuList) {
-        for (boolean[][] tabuMove : tabuList) {
-            if (Arrays.deepEquals(neighbour, tabuMove)) {
-                return true;
-            }
-        }
-        return false;
-    }
     @Override
     protected Result solve() {
-
-        this.theta=1;
-        this.c1=0.8;
-        this.c2=0.3;
-        this.iterationNumber=500;
-        this.populationSize=500;
-        double tabuSearchRate=0.2;
-        double mutationRate=0.1;
+        double hillClimbingRate=0.2;
         ArrayList<Particle> particles= generateRandomParticles(populationSize);
         int iteration=0;
         particles.sort(Comparator.comparingInt(Particle::getFitnessValue));
@@ -180,8 +162,8 @@ public class NQueensPSO extends NQueens{
                     mutate(newParticle);
                     newParticle.fitnessValue=calculateFitness(newParticle.position);
                 }
-                if(Math.random()<tabuSearchRate){
-                    Particle bestNeighbour=rechercheTabou(newParticle,100);
+                if(Math.random()<hillClimbingRate){
+                    Particle bestNeighbour=hillClimbing(newParticle);
                     if(bestNeighbour.getFitnessValue()<newParticle.getFitnessValue()){
                         newParticle=bestNeighbour;
                     }
@@ -194,8 +176,8 @@ public class NQueensPSO extends NQueens{
             }
             iteration++;
             particles.sort(Comparator.comparingInt(Particle::getFitnessValue));
-            gbest=particles.get(0);
-            System.out.println("Iteration : "+iteration + " - "+gbest.getFitnessValue());
+            if(gbest.getFitnessValue()>particles.get(0).getFitnessValue())
+                gbest=particles.get(0);
         }
         return new Result( gbest.position,gbest.getFitnessValue());
     }
