08a330a2430ecf8c4d04c54c02e428870873b92b
damia17 <damiamh17@gmail.com>
2023-05-04 20:32:50 +0100

    Test Code

--- src/main/java/com/example/queens_problem/Testing.java
+++ src/main/java/com/example/queens_problem/Testing.java
@@ -5,190 +5,146 @@ import com.example.queens_problem.logic.*;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
+import com.opencsv.CSVWriter;
 
 public class Testing {
     public static void main(String[] args) {
+////       20 , 40, 60, 80,100
+//        int[] problemSizes = {100};
+//        int[] mutationRates = {30,40, 50,60, 70, 80};
+//        int[] maxIterations = {100, 250, 500,750, 1000,1250};
+//        int[] PopulationSizes = {100, 500,750, 1000,1250};
+//        String GAFilePath = "GAResults.csv";
+//        CSVWriter gaResultsWriter = null;
+//        try {
+//            gaResultsWriter = new CSVWriter(new FileWriter(GAFilePath));
+//            String[] header = {"problem size", "Rate", "Iterations", "Population size","fitness moyenne", "Excution time", "taux de reussite"};
+//            gaResultsWriter.writeNext(header);
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//        for (int problemSize : problemSizes)
+//        {
+//            for (int rate : mutationRates) {
+//                for (int iterations : maxIterations)
+//                {
+//                    for (int PopulationSize: PopulationSizes)
+//                    {
+//                        long tempMoy = 0;
+//                        int tauxResussite = 0;
+//                        int fitnesseMoy = 0;
+//                        System.out.println(problemSize+" , "+rate+" , "+ iterations+" , "+PopulationSize);
+//                        for (int i=0;i<5;i++)
+//                        {
+//                            System.out.println("iteration "+ (i+1));
+//                            long startTime = System.currentTimeMillis();
+//                            NQueensGA nQueens = new NQueensGA(problemSize,PopulationSize,iterations,rate);
+//                            Result result = nQueens.solveAndGetResult();
+//                            long endTime = System.currentTimeMillis();
+//                            long durationInMillis = (endTime - startTime);
+//                            tempMoy += durationInMillis;
+//                            if(result.fitness == 0)
+//                            {
+//                                tauxResussite++;
+//                            }
+//                            fitnesseMoy += result.fitness;
+//                        }
+//                        tempMoy = tempMoy/5;
+//                        tauxResussite = tauxResussite*20;
+//                        fitnesseMoy = fitnesseMoy/5;
+//                        String[] row = {String.valueOf(problemSize), String.valueOf(rate),String.valueOf(iterations), String.valueOf(PopulationSize), String.valueOf(fitnesseMoy),String.valueOf(tempMoy),String.valueOf(tauxResussite)};
+//                        gaResultsWriter.writeNext(row);
+//                        try {
+//                            gaResultsWriter.flush();
+//                        } catch (IOException e) {
+//                            e.printStackTrace();
+//                        }
+//                    }
+//                }
+//            }
+//        }
+//
+//        try {
+//            gaResultsWriter.close();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+        int[] problemSizesPSO = {20,40,60,80,100};
+        int[] maxIterationsPSO = {100, 250, 500,750, 1000,1250};
+        int[] mutationRatesPSO = {5,10, 15,20,25, 30};
+        int[] PopulationSizesPSO = {100, 500,750, 1000,1250};
+        int[] c1 = {1,2,3,4,5};
+        int[] c2 = {1,2,3,4,5};
+        int[] theta = {1,2,3,4,5};
+        String PSOFilePath = "PSOResults.csv";
+        CSVWriter PSOResultsWriter = null;
         try {
-//            printInCSV(27, 30, Algorithm.DFS);
-//            printInCSV(13, 13, Algorithm.BFS);
-//            printInCSV(30, 30, Algorithm.NCH);
-//            printInCSV(29, 30, Algorithm.CCF);
-
-//            printSolutions(29, 30, Algorithm.CCF);
-            printSolutions(30, 30, Algorithm.NCH);
-//            printSolutions(4, 21, Algorithm.DFS);
-//            printSolutions(23, 26, Algorithm.DFS);
-//            printSolutions(4, 12, Algorithm.BFS);
+            PSOResultsWriter = new CSVWriter(new FileWriter(PSOFilePath));
+            String[] header = {"problem size","Iterations", "Population size","Rate","C1","C2","theta", "fitness moyenne", "Excution time", "taux de reussite"};
+            PSOResultsWriter.writeNext(header);
         } catch (IOException e) {
             e.printStackTrace();
         }
-    }
-
-    static void printSolutions(int start, int end, Algorithm algo) throws IOException {
-        FileWriter fw = null;
-        String fileName = "";
-        switch (algo) {
-            case DFS -> {
-                fileName = "resultDFS.csv";
-            }
-            case BFS -> {
-                fileName = "resultBFS.csv";
-            }
-            case CCF -> {
-                fileName = "resultCCF.csv";
-            }
-            case NCH -> {
-                fileName = "resultNCH.csv";
-            }
-        }
-
-        fw = new FileWriter(fileName, true);
-
-
-        ArrayList<Result> resultsArrayList = new ArrayList<>();
-
-
-        for (int i = start; i <= end; i++) {
-            Result results;
-            NQueens nQueens = null;
-            switch (algo) {
-                case DFS -> {
-                    nQueens = new NQueensDFS(i);
-                }
-                case BFS -> {
-                    nQueens = new NQueensBFS(i);
-
-                }
-                case CCF -> {
-                    nQueens = new NQueensCCF(i);
-
-                }
-                case NCH -> {
-                    nQueens = new NQueensNCH(i);
-                }
-            }
-            results = nQueens.solveAndGetResult();
-
-            resultsArrayList.add(results);
-        }
-        fw.append(",");
-
-        for (int j = 0; j < resultsArrayList.size(); j++) {
-            if (j == resultsArrayList.size() - 1) {
-                fw.append(String.valueOf(resultsArrayList.get(j).solution.length)).append("\n");
-            } else {
-                fw.append(String.valueOf(resultsArrayList.get(j).solution.length)).append(",");
-
-            }
-
-
-        }
-        fw.append("Solutions,");
-        for (int j = 0; j < resultsArrayList.size(); j++) {
-            fw.append('{');
-            boolean[][] solution = resultsArrayList.get(j).solution;
-            for (int i = 0; i < solution.length; i++) {
-                for (int h = 0; h < solution[i].length; h++) {
-                    if (solution[i][h]) {
-                        if (i == solution.length - 1) {
-                            fw.append(String.valueOf(h));
-
-                        } else {
-                            fw.append(String.valueOf(h)).append(';');
+        for (int problemSize : problemSizesPSO)
+        {
+            for (int PopulationSize : PopulationSizesPSO)
+            {
+                for (int maxGeneration : maxIterationsPSO)
+                {
+                    for (int rate : mutationRatesPSO)
+                    {
+                        for (int c : c1)
+                        {
+                            for (int cc : c2)
+                            {
+                                for (int t : theta)
+                                {
+                                    long tempMoy = 0;
+                                    int tauxResussite = 0;
+                                    int fitnesseMoy = 0;
+                                    System.out.println(problemSize+" , "+PopulationSize+" , "+maxGeneration+" , "+rate+" , "+c+ " , "+cc+" , "+ t);
+                                    for (int i=0;i<5;i++)
+                                    {
+                                        System.out.println("iteration "+ (i+1));
+                                        long startTime = System.currentTimeMillis();
+                                        NQueensPSO nQueens =new NQueensPSO(problemSize,t,c,cc,PopulationSize,maxGeneration, rate);
+                                        Result result = nQueens.solveAndGetResult();
+                                        long endTime = System.currentTimeMillis();
+                                        long durationInMillis = (endTime - startTime);
+                                        tempMoy += durationInMillis;
+                                        if(result.fitness == 0)
+                                        {
+                                            tauxResussite++;
+                                        }
+                                        fitnesseMoy += result.fitness;
+                                    }
+                                    tempMoy = tempMoy/5;
+                                    tauxResussite = tauxResussite*20;
+                                    fitnesseMoy = fitnesseMoy/5;
+                                    String[] row = {String.valueOf(problemSize),String.valueOf(PopulationSize),String.valueOf(maxGeneration),String.valueOf(rate),String.valueOf(c),String.valueOf(cc), String.valueOf(t),String.valueOf(fitnesseMoy),String.valueOf(tempMoy),String.valueOf(tauxResussite)};
+                                    PSOResultsWriter.writeNext(row);
+                                    try {
+                                        PSOResultsWriter.flush();
+                                    } catch (IOException e) {
+                                        e.printStackTrace();
+                                    }
+                                }
+                            }
                         }
                     }
                 }
+
             }
-            fw.append("},");
         }
-        fw.close();
+
+        try {
+            PSOResultsWriter.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 
-    static void printInCSV(int start, int end, Algorithm algo) throws IOException {
-        FileWriter fw = null;
-        String fileName = "";
-        switch (algo) {
-            case DFS -> {
-                fileName = "resultDFS.csv";
-            }
-            case BFS -> {
-                fileName = "resultBFS.csv";
-            }
-            case CCF -> {
-                fileName = "resultCCF.csv";
-            }
-            case NCH -> {
-                fileName = "resultNCH.csv";
-            }
-        }
-
-        fw = new FileWriter(fileName, true);
-
-
-        ArrayList<Result> resultsArrayList = new ArrayList<>();
-
-
-        for (int i = start; i <= end; i++) {
-            Result results;
-            NQueens nQueens = null;
-            switch (algo) {
-                case DFS -> {
-                    nQueens = new NQueensDFS(i);
-                }
-                case BFS -> {
-                    nQueens = new NQueensBFS(i);
-
-                }
-                case CCF -> {
-                    nQueens = new NQueensCCF(i);
-
-                }
-                case NCH -> {
-                    nQueens = new NQueensNCH(i);
-                }
-            }
-            results = nQueens.solveAndGetResult();
-
-            resultsArrayList.add(results);
-        }
-        fw.append(",");
-
-        for (int j = 0; j < resultsArrayList.size(); j++) {
-            if (j == resultsArrayList.size() - 1) {
-                fw.append(String.valueOf(resultsArrayList.get(j).solution.length)).append("\n");
-            } else {
-                fw.append(String.valueOf(resultsArrayList.get(j).solution.length)).append(",");
-
-            }
-        }
-        fw.append("Execution Time,");
-        for (int j = 0; j < resultsArrayList.size(); j++) {
-            if (j == resultsArrayList.size() - 1) {
-                fw.append(String.valueOf(resultsArrayList.get(j).executionTime)).append("\n");
-            } else {
-                fw.append(String.valueOf(resultsArrayList.get(j).executionTime)).append(",");
-            }
-        }
-        fw.append("Generated Nodes,");
-
-        for (int j = 0; j < resultsArrayList.size(); j++) {
-            if (j == resultsArrayList.size() - 1) {
-                fw.append(String.valueOf(resultsArrayList.get(j).generatedNodes)).append("\n");
-            } else {
-                fw.append(String.valueOf(resultsArrayList.get(j).generatedNodes)).append(",");
-            }
-        }
-        fw.append("Developed Nodes,");
-
-        for (int j = 0; j < resultsArrayList.size(); j++) {
-            if (j == resultsArrayList.size() - 1) {
-                fw.append(String.valueOf(resultsArrayList.get(j).developedNodes)).append("\n");
-            } else {
-                fw.append(String.valueOf(resultsArrayList.get(j).developedNodes)).append(",");
-            }
-        }
-        fw.close();
-    }
 }
 
 
